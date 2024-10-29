33f30f31d9f44bdc39d94c57b1ea682353a083aa
sejongmin <jmoh7480@gmail.com>
2024-10-30 01:32:13 +0900

    Feat/ image name으로 찾기

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,9 +1,8 @@
-import json
 import pandas as pd
 import streamlit as st
-from pathlib import Path
 
 import visualize_ocr
+import load_json
 
 class_colors = {
     'paragraph': (255, 241, 168),
@@ -11,78 +10,10 @@ class_colors = {
     'chars': (238, 100, 69)
 }
 
-def read_json(filename):
-    with Path(filename).open(encoding='utf8') as handle:
-        ann = json.load(handle)
-    return ann
-
-@st.cache_data
-def load_train_df(json_path='./code/data/chinese_receipt/ufo/train.json'):
-    data = {}
-    data['images'] = {}
-    json_data = read_json(json_path)
-    images = list(json_data['images'].items())
-    data['images'].update(dict(images))
-
-    train_df = pd.DataFrame()
-    image_ids = []
-    word_ids = []
-    image_heights = []
-    image_widths = []
-    transcriptions = []
-    x1, x2, x3, x4 = [], [], [], []
-    y1, y2, y3, y4 = [], [], [], []
-
-    for image_key, image_value in list(data["images"].items()):
-        file_name = image_key
-        word_ann = image_value['words']
-        for word in word_ann.values():
-            image_ids.append(file_name)
-            word_ids.append(word)
-            image_heights.append(image_value['img_h'])
-            image_widths.append(image_value['img_w'])
-            transcriptions.append(word['transcription'])
-            x1.append(float(word['points'][0][0]))
-            y1.append(float(word['points'][0][1]))
-            x2.append(float(word['points'][1][0]))
-            y2.append(float(word['points'][1][1]))
-            x3.append(float(word['points'][2][0]))
-            y3.append(float(word['points'][2][1]))
-            x4.append(float(word['points'][3][0]))
-            y4.append(float(word['points'][3][1]))
-
-    train_df['image_id'] = image_ids
-    train_df['word_ids'] = word_ids
-    train_df['img_w'] = image_widths
-    train_df['img_h'] = image_heights
-    train_df['transcriptions'] = transcriptions
-    train_df['x1'] = x1
-    train_df['y1'] = y1
-    train_df['x2'] = x2
-    train_df['y2'] = y2
-    train_df['x3'] = x3
-    train_df['y3'] = y3
-    train_df['x4'] = x4
-    train_df['y4'] = y4
-
-    bbox_df = pd.DataFrame()
-    bbox_df['word_ids'] = train_df['word_ids'].values
-    bbox_df['transcriptions'] = train_df['transcriptions'].values
-    bbox_df['x1'] = train_df['x1'].values
-    bbox_df['y1'] = train_df['y1'].values
-    bbox_df['x2'] = train_df['x2'].values
-    bbox_df['y2'] = train_df['y2'].values
-    bbox_df['x3'] = train_df['x3'].values
-    bbox_df['y3'] = train_df['y3'].values
-    bbox_df['x4'] = train_df['x4'].values
-    bbox_df['y4'] = train_df['y4'].values
-
-    return train_df, bbox_df
-        
 if "train_df" not in st.session_state:
     st.session_state.train_df = pd.DataFrame()
 if "bbox_df" not in st.session_state:
-    st.session_state.train_df = pd.DataFrame()
+    st.session_state.bbox_df = pd.DataFrame()
 
 st.sidebar.success("CV16 오늘도 화이팅하조!")
 with st.sidebar.form(key="json_form"):
@@ -90,22 +21,30 @@ with st.sidebar.form(key="json_form"):
     submit_button = st.form_submit_button("OK")
     if submit_button:
         try:
-            st.session_state.train_df, st.session_state.bbox_df = load_train_df(json_path)
+            st.session_state.train_df, st.session_state.bbox_df = load_json.load_df(json_path)
             st.sidebar.success("json file load successed :)")
-        except Exception:
-            st.session_state.train_df, st.session_state.bbox_df = load_train_df()
+        except Exception as e:
             st.sidebar.error("json file load failed :(")
 
 st.markdown("<h2 style='text-align: center;'>OCR</h2>", unsafe_allow_html=True)
 option = st.sidebar.radio("option", ["train images", "test images"])
 if option == "train images" and not st.session_state.train_df.empty:
     st.session_state.image_ids = [img_id for img_id in st.session_state.train_df.groupby("image_id")["image_id"].first().tolist()]
-    image_index = st.sidebar.slider('Select image id', 0, len(st.session_state.image_ids), 0)
-    image_index_input = st.sidebar.number_input('Enter image count', min_value=0, max_value=len(st.session_state.image_ids), value=image_index, step=1)
+    image_count = st.sidebar.slider('Select image count', 1, 4, 1)
+    image_index = st.sidebar.slider('Select image id', 0, len(st.session_state.image_ids)-image_count, 0)
+    image_index_input = st.sidebar.number_input('Enter image count', min_value=0, max_value=len(st.session_state.image_ids)-image_count, value=image_index, step=image_count)
     if image_index != image_index_input:
         image_index = image_index_input
-    image_id = st.session_state.image_ids[image_index]
-    visualize_ocr.show(st.session_state.train_df, image_id, json_path, class_colors)
+    image_ids = [st.session_state.image_ids[i] for i in range(image_index, image_index + image_count)]
+    with st.sidebar.form(key="image name form"):
+        image_name = st.text_input("image_name")
+        submit_button = st.form_submit_button("OK")
+        if submit_button:
+            try:
+                image_ids = [image_name]
+            except Exception as e:
+                st.sidebar.error("failed :(")
+    visualize_ocr.show(st.session_state.train_df, image_ids, json_path, class_colors)
 elif option == "test images" and not st.session_state.test_df.empty:
     st.session_state.image_ids = [img_id for img_id in st.session_state.test_df.groupby("image_id")["image_id"].first().tolist()]
 else:
--- streamlit/visualize_ocr.py
+++ streamlit/visualize_ocr.py
@@ -9,27 +9,29 @@ def draw_bbox(image, coordinates, color=(0, 255, 0), thickness=2):
         end_point = tuple(points[(i + 1) % len(points)])
         cv2.line(image, start_point, end_point, color, thickness)
 
-def show(df, image_id, json_path, class_colors):
-    image_data = df[df['image_id'] == image_id]
+def show(df, image_ids, json_path, class_colors):
+    cols = st.columns(len(image_ids))
 
-    base_path = '/'.join(json_path.split('/')[:-2]) + "/img/train/"
-    image = cv2.imread(base_path + image_id)
-    print(base_path + image_id)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    for i, image_id in enumerate(image_ids):
+        image_data = df[df['image_id'] == image_id]
 
-    display_w, display_h = image.shape[1], image.shape[0]
-    for _, row in image_data.iterrows():
-        try:
-            img_w, img_h = row['img_w'], row['img_h']
-            color = class_colors.get('word', (255, 255, 255))
-            scaled_coordinates = [
-                int(row['x1'] * display_w / img_w), int(row['y1'] * display_h / img_h),
-                int(row['x2'] * display_w / img_w), int(row['y2'] * display_h / img_h),
-                int(row['x3'] * display_w / img_w), int(row['y3'] * display_h / img_h),
-                int(row['x4'] * display_w / img_w), int(row['y4'] * display_h / img_h)
-            ]
-            draw_bbox(image, scaled_coordinates, color=color, thickness=2)
-        except Exception as e:
-            st.write(e)
+        base_path = '/'.join(json_path.split('/')[:-2]) + "/img/train/"
+        image = cv2.imread(base_path + image_id)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    st.image(image, caption=image_id, use_column_width=True)
+        display_w, display_h = image.shape[1], image.shape[0]
+        for _, row in image_data.iterrows():
+            try:
+                img_w, img_h = row['img_w'], row['img_h']
+                color = class_colors.get('word', (255, 255, 255))
+                scaled_coordinates = [
+                    int(row['x1'] * display_w / img_w), int(row['y1'] * display_h / img_h),
+                    int(row['x2'] * display_w / img_w), int(row['y2'] * display_h / img_h),
+                    int(row['x3'] * display_w / img_w), int(row['y3'] * display_h / img_h),
+                    int(row['x4'] * display_w / img_w), int(row['y4'] * display_h / img_h)
+                ]
+                draw_bbox(image, scaled_coordinates, color=color, thickness=2)
+            except Exception as e:
+                st.write(e)
+        with cols[i]:
+            st.image(image, caption=image_id, use_column_width=True)
