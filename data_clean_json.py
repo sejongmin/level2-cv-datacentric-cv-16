4e7c89e74ac58b7bcbd042d0a3763a4130eaad1f
kkh090 <kihoon090@gmail.com>
2024-10-29 22:42:22 -0900

    Feat/ data_clean_json.py checking for ufo format

--- data_clean_json.py
+++ data_clean_json.py
@@ -1,7 +1,24 @@
 import json 
 from pathlib import Path
 
-def create_json_without_(ufo_path, output_path, ):
+def check_for_key():
+    pass
+
+def create_json_without_(ufo_path: str, output_path: str, key: str=""):
     with Path(ufo_path).open(encoding='utf8') as f:
         ufo_data = json.load(f)
         
+    print(type(ufo_data))
+    print(ufo_data.keys())
+    print(ufo_data['images'].keys())
+    print(ufo_data['images']['extractor.vi.in_house.appen_001105_page0001.jpg'].keys())
+    print(ufo_data['images']['extractor.vi.in_house.appen_001105_page0001.jpg']['words'].keys())
+    print(ufo_data['images']['extractor.vi.in_house.appen_001105_page0001.jpg']['words']['0001'].keys())
+    print(ufo_data['images']['extractor.vi.in_house.appen_001105_page0001.jpg']['words']['0001']['transcription'])
+    
+    
+
+if __name__=="__main__":
+    create_json_without_(ufo_path="data/vietnamese_receipt/ufo/train.json", output_path="./result.json", key="")
+
+    
