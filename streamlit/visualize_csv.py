9fc334b9f2c68cbfe34a49adb9214b405d4b1cb7
sejongmin <jmoh7480@gmail.com>
2024-10-31 11:05:22 +0900

    Fix/ 오류처리

--- streamlit/visualize_csv.py
+++ streamlit/visualize_csv.py
@@ -10,24 +10,23 @@ def draw_bbox(image, coordinates, color=(0, 255, 0), thickness=2):
         cv2.line(image, start_point, end_point, color, thickness)
 
 def show(df, image_ids):
-    color = (208, 56, 78)
-    nation_dict = {
-        'vi': 'vietnamese_receipt',
-        'th': 'thai_receipt',
-        'zh': 'chinese_receipt',
-        'ja': 'japanese_receipt',
-    }
-    cols = st.columns(len(image_ids))
-    
-    for i, image_id in enumerate(image_ids):
-        image_data = df[df['image_id'] == image_id]
-        receipt = nation_dict.get(image_id.split('.')[1])
-        image = cv2.imread(f'data/{receipt}/img/test/' + image_id)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    try:
+        color = (208, 56, 78)
+        nation_dict = {
+            'vi': 'vietnamese_receipt',
+            'th': 'thai_receipt',
+            'zh': 'chinese_receipt',
+            'ja': 'japanese_receipt',
+        }
+        cols = st.columns(len(image_ids))
+        
+        for i, image_id in enumerate(image_ids):
+            image_data = df[df['image_id'] == image_id]
+            receipt = nation_dict.get(image_id.split('.')[1])
+            image = cv2.imread(f'data/{receipt}/img/test/' + image_id)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        for _, row in image_data.iterrows():
-            print(row)
-            try:
+            for _, row in image_data.iterrows():
                 scaled_coordinates = [
                     int(row['x1']), int(row['y1']),
                     int(row['x2']), int(row['y2']),
@@ -35,7 +34,7 @@ def show(df, image_ids):
                     int(row['x4']), int(row['y4'])
                 ]
                 draw_bbox(image, scaled_coordinates, color=color, thickness=2)
-            except Exception as e:
-                st.write(e)
-        with cols[i]:
-            st.image(image, caption=image_id, use_column_width=True)
+            with cols[i]:
+                st.image(image, caption=image_id, use_column_width=True)
+    except Exception as e:
+        st.write(e)
--- streamlit/visualize_json.py
+++ streamlit/visualize_json.py
@@ -10,20 +10,20 @@ def draw_bbox(image, coordinates, color=(0, 255, 0), thickness=2):
         cv2.line(image, start_point, end_point, color, thickness)
 
 def show(df, image_ids, json_path):
-    color = (208, 56, 78)
-    cols = st.columns(len(image_ids))
-    json_path = json_path.split('/')
-    mode = json_path[-1].split('.')[0]
+    try:
+        color = (208, 56, 78)
+        cols = st.columns(len(image_ids))
+        json_path = json_path.split('/')
+        mode = json_path[-1].split('.')[0]
 
-    for i, image_id in enumerate(image_ids):
-        image_data = df[df['image_id'] == image_id]
-        base_path = '/'.join(json_path[:-2]) + f"/img/{mode}/"
-        image = cv2.imread(base_path + image_id)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        for i, image_id in enumerate(image_ids):
+            image_data = df[df['image_id'] == image_id]
+            base_path = '/'.join(json_path[:-2]) + f"/img/{mode}/"
+            image = cv2.imread(base_path + image_id)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        display_w, display_h = image.shape[1], image.shape[0]
-        for _, row in image_data.iterrows():
-            try:
+            display_w, display_h = image.shape[1], image.shape[0]
+            for _, row in image_data.iterrows():
                 if mode == "train":
                     img_w, img_h = row['img_w'], row['img_h']
                     scaled_coordinates = [
@@ -33,7 +33,8 @@ def show(df, image_ids, json_path):
                         int(row['x4'] * display_w / img_w), int(row['y4'] * display_h / img_h)
                     ]
                     draw_bbox(image, scaled_coordinates, color=color, thickness=2)
-            except Exception as e:
-                st.write(e)
         with cols[i]:
             st.image(image, caption=image_id, use_column_width=True)
+    except Exception as e:
+        st.write(e)
+        
